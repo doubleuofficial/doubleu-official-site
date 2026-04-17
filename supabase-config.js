9f0567d5960a9d2760e5507794409603a8353613
doubleuofficial <doubleu.official405@gmail.com>
2026-04-17 10:49:06 -0500

    minimum 	o

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://xpwgzassajhmlqrjyyon.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhwd2d6YXNzYWpobWxxcmp5eW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMzIyNTYsImV4cCI6MjA5MTgwODI1Nn0.yuizzF45EItmSe224LutMGNoGcUsgLCUWu-zXVU6o9g'
-export const supabase = createClient(supabaseUrl, supabaseKey)
